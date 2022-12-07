ca68d46ba965e5b6cace89d0f021021cb913bdfd
Tari Richárd <54990880+comradericsi@users.noreply.github.com>
2022-12-07 17:02:25 +0100

    Update contact.tpl.php

--- templates/pages/contact.tpl.php
+++ templates/pages/contact.tpl.php
@@ -1,11 +1,16 @@
-<form action = "?oldal=data" method = "post" class=" border bg-light">
+<script type="text/javascript" src="logicals/check.js"></script>
+
+<form action = "?oldal=data"  class=" border bg-light" onsubmit="return ellenoriz();" method = "post">
   <div class="ms-4">
       <fieldset>
-        <legend>Bejlentkezés</legend>
+        <legend>Email írás:</legend>
         <br>
-        <input type="text" name="felhasznalo" placeholder="felhasználó" required><br><br>
-        <input type="password" name="jelszo" placeholder="jelszó" required><br><br>
-        <input type="submit" name="belepes" value="Sign In">
+		<label>Email cím:</label><br>
+        <input type="email" id="email" name="email" placeholder="email" required><br><br>
+		<label>Üzenet (kötelező): </label><br>
+		<textarea id="szoveg" name="szoveg" cols="40" rows="10"></textarea><br> 
+		<button onclick="ellenoriz();" type="button">Ellenőriz</button>
+		<input id="kuld" type="submit" value="Küld">
         <br>&nbsp;
       </fieldset>
   </div>
